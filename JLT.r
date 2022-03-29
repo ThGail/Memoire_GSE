4b05f10f1152523b39d1add9f85482be649d6664
Thibault Gaillard <thibault.gaillard1@gmail.com>
2022-03-29 21:22:43 +0200

    JLT fonction with matrix

--- JLT.r
+++ JLT.r
@@ -1,3 +1,5 @@
+rm(list = ls())
+
 # exemple de matrice de transition de rating
 Q = matrix(c(91.05,8.25,0.6,0.07,0.02,0,0,0,0,
              0.86,89.59,8.96,0.45,0.07,0.04,0.02,0,0.02,
@@ -54,33 +56,112 @@ B_fct <- function(u, param, dj) {
 }
 
 # On suppose que le mouvement brownien est standard
-pit_fct <- function(N, t, param) {
-  k <- param[1]
-  mu <- param[2]
-  sigma <- param[3]
-  pi0 <- param[4]
-  pi_t <- pi0
+pit_fct <- function(N, t, param)
+{
+  #k <- param[,1]
+  #mu <- param[,2]
+  #sigma <- param[,3]
+  #pi_t <- param[,4]
+  
+  k <- matrix(rep(param_test[,1], N), nrow = 8)
+  mu <- matrix(rep(param_test[,2], N), nrow = 8)
+  sigma <- matrix(rep(param_test[,3], N), nrow = 8)
+  pi_t <- matrix(rep(param_test[,4], N), nrow = 8)
+  
   if (t==0) {return(pi_t)}
-  for (i in 1:t) {
-    pi_t <- abs(pi_t + k * (mu - pi_t) + sigma * sqrt(pi_t) * rnorm(N, 0, 1))
+  for (i in 1:t)
+  {
+    #pi_t <- abs(pi_t + k * (mu - pi_t) + sigma * sqrt(pi_t) * rnorm(N, 0, 1))
+    #pi_t <- abs(pi_t + k * (mu - pi_t) + (sigma * sqrt(pi_t)) %o% rnorm(N, 0, 1))
+    pi_t <- abs(pi_t + k * (mu - pi_t) + t(t(sigma * sqrt(pi_t)) * rnorm(N, 0, 1)))
+    print(i)
+    print(pi_t)
   }
   return(pi_t)
 }
 
+calcul_esp <- function(pi_t)
+{
+  
+}
+
 proba_defaut_i <- function(N, t, TT, param, M, D, i){
-  k <- param[1]
-  mu <- param[2]
-  sigma <- param[3]
-  lambda0 <- param[4]
   
   invM <- solve(M) # le mettre en param ?
   K <- length(D[1,])
   
   sum = 0
   for (j in (1:(K-1))){
-    esp <- A_fct(TT-t, param, D[j,j])*exp(-B_fct(TT-t, param, D[j,j])*pit_fct(N,t,param))
+    esp <- (1/N)*rowSums(A_fct(TT-t, param, D[j,j])*exp(-B_fct(TT-t, param, D[j,j])*pit_fct(N,t,param)))
+    print('esp')
+    print(esp)
     sum = sum + M[i,j]*invM[j,K]*(esp-1)
   }
   
   return(sum)
 }
+
+######### CALIBRAGE#########
+#On considere param comme une matrice, chaque ligne represente les parametres d une classe de rating
+#Sachant qu'il y a 8 classes de rating et 4 parametres pour chacune d entre elle,
+#La dimension de la matrice param est 8 x 4
+#Les parametres sont c(kAAA, kAA, kA, kBaa, kBa, kB, kCaa, kCa-C, muAAA, muAA, muA, muBaa, muBa, muB, muCaa, muCaC, sigma, pi0)
+
+#### TEST ( a garder temporairement) ####
+param_test <- matrix(c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.3, 0.2, 0.5, 0.4, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.6, 0.5, 0.5, 0.5, 0.7, 0.5, 0.5, 0.5, 0.8, 0.5, 0.5, 0.5), nrow =8)
+print(param_test)
+
+n <- 2
+k_test <- matrix(rep(param_test[,1], n), nrow = 8)
+mu_test <- matrix(rep(param_test[,2], n), nrow = 8)
+sigma_test <- matrix(rep(param_test[,3], n), nrow = 8)
+pi_t_test <- matrix(rep(param_test[,4], n), nrow = 8)
+
+print(pi_t_test)
+
+#pi_t <- abs(pi_t + k * (mu - pi_t) + sigma * sqrt(pi_t) * rnorm(N, 0, 1))
+random <- rnorm(n,0,1)
+print(random)
+pi_t_test <- abs(pi_t_test + k_test * (mu_test - pi_t_test) + t(t(sigma_test * sqrt(pi_t_test)) * random))
+random <- rnorm(n,0,1)
+pi_t_test <- abs(pi_t_test + k_test * (mu_test - pi_t_test) + t(t(sigma_test * sqrt(pi_t_test)) * random))
+random <- rnorm(n,0,1)
+pi_t_test <- abs(pi_t_test + k_test * (mu_test - pi_t_test) + t(t(sigma_test * sqrt(pi_t_test)) * random))
+
+print(temp)
+
+k_test2 <- param_test[,1]
+mu_test2 <- param_test[,2]
+sigma_test2 <- param_test[,3]
+pi_t_test2 <- param_test[,4]
+print(abs(pi_t_test2) + k_test2 * (mu_test2 - pi_t_test2))
+print(sigma_test2)
+temp2 <- abs(pi_t_test2) + k_test2 * (mu_test2 - pi_t_test2) + (sigma_test2 * sqrt(pi_t_test2)) %o% random
+print(temp2)
+
+
+
+
+
+
+
+
+##### Calibrage des paramètres ####
+hist(pit_fct(1000, 3, param_test), freq=FALSE, main='Histogramme des simulations (param random)', xlab='Simulation des lambda t = 3')
+
+Ecart_JLT <- function(param){
+  e <- 0
+  for (t in 0:TT){
+    spread <- Prix_Spread_fct(0,t,param,LGD)
+    print(t)
+    #print(spread)
+    print()
+    print(spread-spread_mkt)
+    e <- e + (spread-spread_mkt)^2
+    #print(e)
+  }
+  return(e)
+}
+
+test <- c(1,2,3,4)
+print(test[1:3])
