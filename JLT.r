3032752ad8f66a4a8d4d50a20577af3d88d735c8
Mengru CHEN <99873364+MengruCHEN@users.noreply.github.com>
2022-04-05 16:27:01 +0200

    calibrage JLT + plot (until l. 183)

--- JLT.r
+++ JLT.r
@@ -12,7 +12,7 @@ Maturite <- as.numeric(sheetDGlo$`Courbe des taux ZC`[7:156])
 TauxZC    <- as.numeric(sheetDGlo$...2 [7:156])
 SpreadMarket  <- as.numeric(sheetDGlo$...15[3:9])
 
-
+LGD = 0.3
 
 # exemple de matrice de transition de rating
 Q = matrix(c(91.06,8.25,0.6,0.07,0.02,0,0,0,0,
@@ -57,7 +57,7 @@ A_fct <- function(u, param, dj) {
   k <- param[,1]
   mu <- param[,2]
   sigma <- param[,3]
-  ga <- sqrt(k**2 + 2 * dj * sigma**2)
+  ga <- sqrt(k**2 - 2 * dj * sigma**2)
   
   return(((2 * ga * exp((k + ga) * (u / 2))) / ((k + ga) * (exp(ga * u) - 1) + 2 * ga))**((2 * k * mu) / (sigma**2)))
 }
@@ -66,7 +66,7 @@ B_fct <- function(u, param, dj) {
   k <- param[,1]
   mu <- param[,2]
   sigma <- param[,3]
-  ga <- sqrt(k**2 + 2 * dj * sigma**2)
+  ga <- sqrt(k**2 - 2 * dj * sigma**2)
 
   return((- 2 * dj * (exp(ga * u) - 1)) / ((k + ga) * (exp(ga * u) - 1) + 2 * ga))
 }
@@ -86,33 +86,59 @@ pit_fct <- function(N, t, param) {
 }
 
 proba_defaut_i <- function(N, t, TT, param, M, D, i){
+# pour un rating donnée, on fait N simulations
   
   invM <- solve(M) # le mettre en param ?
   K <- length(D[1,])
   
   sum = 0
-  
   for (j in (1:(K-1))){
-    esp <- rowMeans(A_fct(TT-t, param, D[j,j])*exp(-B_fct(TT-t, param, D[j,j])*pit_fct(N,t,param)))[i]
-    sum = sum + M[i,j]*invM[i,j]*(esp-1)
+    esp <- rowMeans(A_fct(TT-t, param, D[j,j])*exp(-B_fct(TT-t, param, D[j,j])*pit_fct(N,t,param)))[i] # enlever rowMeans pour avoir les trajectoires ?
+    sum <- sum + M[i,j]*invM[j,K]*(esp-1)
   }
   return(sum)
-    
+}
+
+proba_defaut_i_calibrage <- function(TT, param, M, D, i){
+# pour un rating donnée, et une maturité TT donnée, on a juste besion de t=0
+  
+  invM <- solve(M)
+  K <- length(D[1,])
+  pi0 <- param_test[,4]
+  
+  sum <- 0
+  for (j in (1:(K-1))){
+    esp <- A_fct(TT, param, D[j,j])[i]*exp(-B_fct(TT, param, D[j,j])[i]*pi0[i])
+    sum <- sum + M[i,j]*invM[j,K]*(esp-1)
+  }
+  return(sum)
 }
 
 spread_i_fct <- function(N, t, TT, param, M, D, i){
-  if (t==TT){return(0)} # a verifier au temps t==TT
-  if (1- LGD * proba_defaut_i(N, t, TT, param, M, D, i)<=0){return(0)} # à vérifier la condition
+  # if (t==TT){return(0)} # a verifier au temps t==TT
+  # if (1- LGD * proba_defaut_i(N, t, TT, param, M, D, i)<=0){return(0)} # à vérifier la condition
   return(-(1/(TT-t))*log(1- LGD * proba_defaut_i(N, t, TT, param, M, D, i)))
 }
 
-# test pour le spread AAA
-# pb : s'arrete à la maturité 25, au delà ne fonctionne pas tjrs
-L <- c()
-for (t in 0:25){
-  L <- c(L,spread_fct(100, 0, t, param_test, M, D, 1,LGD))
-} 
-plot(L)
+spread_i_calibrage <- function(TT, param, M, D, i, LGD){
+  return(-log(1 - LGD * proba_defaut_i_calibrage(TT, param, M, D, i))/TT)
+}
+
+# fonction objective à minimiser
+# à minimiseer sur la maturité (ici 1 an ?) et sur les ratings
+# un seul appel à cette fonction necessaire pour le calibrage 
+# à calibrer K fois les paramètres k mu et pi0
+# et 1 fois le paramètre sigma (car la volatilité est supposé constante)
+Ecart_JLT <- function(param){
+  e <- 0
+  for (t in 1:TT){ for (i in 1:6){
+    # spread <- spread_i_fct(1000, t, TT, param_test, M, D, i,LGD)
+    spread <- spread_i_calibrage(t, param, M, D, i, LGD)
+    e <- e + (spread-SpreadMarket[i])^2
+  }}
+  return(e)
+} ## parametre sur le rating
+
 
 ######### CALIBRAGE#########
 #On considere param comme une matrice, chaque ligne represente les parametres d une classe de rating
@@ -120,13 +146,45 @@ plot(L)
 #La dimension de la matrice param est 8 x 4
 #Les parametres sont c(kAAA, kAA, kA, kBaa, kBa, kB, kCaa, kCa-C, muAAA, muAA, muA, muBaa, muBa, muB, muCaa, muCaC, sigma, pi0)
 
-#### TEST ( a garder temporairement) ####
+# param a garder temporairement
+# les params du CIR
+param_test <- matrix(c(0.327, 0.291, 0.200, 0.060, 0.004, 0.003, 0.003, 0.003, 0.197, 0.325, 0.574, 0.543, 1.628, 2.018, 2.000, 2.000, 8.727, 9.614, 9.367, 1.411, 0.001, 0.001, 0.001, 0.001,0.003, 0.001, 0.001, 0.015, 0.054, 0.055, 0.055, 0.055), nrow =8)
+# les params hasards
 param_test <- matrix(c(2, 1.5, 2, 1.5, 2, 1.5, 2, 1.5, 0.3, 0.9, 0.5, 0.4, 0.5, 0.5, 0.5, 0.5, 0.5, 0.9, 0.9, 0.5, 0.6, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1), nrow =8)
-print(param_test)
 
-#### TEST ####
+N <- 1000
+TT <- 5 # quelle maturité prendre ?
+LB <- matrix(c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), nrow = 8)
+UB <- matrix(c(9, 9, 9, 9, 9, 9, 9, 9, 20, 20, 20, 20, 20, 20, 20, 20, 15, 15, 15, 15, 15, 15, 15, 15, 1, 1, 1, 1, 1, 1, 1, 1), nrow = 8)
+param_tt <- hjkb(param_test,Ecart_JLT,lower=LB,upper=UB)$par
+
+# nlminb(start = param_test,Ecart_JLT,lower=LB,upper=UB)
+# optim(par=param_test,fn=Ecart_JLT,lower=LB,upper=UB,method="L-BFGS-B")
+
+spreadAAA <- c()
+spreadAA <- c()
+spreadA <- c()
+spreadBBB <- c()
+spreadBB <- c()
+spreadB <- c()
+for (t in 0:20) {
+  spreadAAA <- c(spreadAAA, spread_i_calibrage(t, param_tt, M, D, 1,LGD))
+  spreadAA <- c(spreadAA, spread_i_calibrage(t, param_tt, M, D, 2,LGD))
+  spreadA <- c(spreadA, spread_i_calibrage(t, param_tt, M, D, 3,LGD))
+  spreadBBB <- c(spreadBBB, spread_i_calibrage(t, param_tt, M, D, 4,LGD))
+  spreadBB <- c(spreadBB, spread_i_calibrage(t, param_tt, M, D, 5,LGD))
+  spreadB <- c(spreadB, spread_i_calibrage(t, param_tt, M, D, 6,LGD))
+}
+plot(spreadB, main='Spread', type='l', ylab="Spread", ylim=c(0,0.03), xlab="Maturité", col='purple')
+lines(spreadAAA, col='red')
+lines(spreadAA, col='orange')
+lines(spreadA, col='brown')
+lines(spreadBBB, col='lightblue')
+lines(spreadBB, col='blue')
+
+
+######### TEST #########
 n <- 2
-
 hist(pit_fct(100, 3, param_test), freq=FALSE, main='Histogramme des simulations (param random)', xlab='Simulation des lambda t = 3')
 random <- rnorm(n,0,1)
 print(random)
@@ -135,28 +193,6 @@ k_test <- matrix(rep(param_test[,1], n), nrow = 8)
 mu_test <- matrix(rep(param_test[,2], n), nrow = 8)
 sigma_test <- matrix(rep(param_test[,3], n), nrow = 8)
 pi_t_test <- matrix(rep(param_test[,4], n), nrow = 8)
-
-
-
-
-#Il y a un problème dans la fonction A !!!!
-(1/N)*rowSums(A_fct(TT-t, param, D[j,j])*exp(-B_fct(TT-t, param, D[j,j])*pit_fct(N,t,param)))
-
-ga <- sqrt(k_test^2 + 2 * D[1,1] * sigma_test^2)
-(2 * ga * exp((k_test + ga) * (2 / 2)) / ((k_test + ga) * (exp(ga * 2) - 1) + 2 * ga))**((2 * k_test * mu_test) / (sigma_test**2))
-print(A_fct(2, param_test, D[1,1]))
-
-print(B_fct(2, param_test, D[1,1]))
-k_test**2 + 2 * D[1,1] * sigma_test**2
-print(pi_t_test)
-
-LGD <- 0.6
-plot(spread_fct(1000, 3, 5, param_test, M, D, 1))
-
-print(proba_defaut_i(2, 3, 5, param_test, M, D, 1))
-
-
-
 
 Ecart_JLT <- function(param){
   e <- 0
@@ -167,49 +203,7 @@ Ecart_JLT <- function(param){
   return(e)
 }
 
-#====== ESSAI =====
-LGD <- 0.3 # car 1-R=LGD
-spread_mkt <- SpreadMarket[6]
-TT <- 15
-Ecart_CIR(param_test)
-
-LB = c(-2,0,0,0)
-UB = c(2,5,1,1)
-param <- hjkb(c(0.2,0.2,0.2,0.2),Ecart_CIR,lower=LB,upper=UB)$par
-
-
-#### fonction objective à minimiser ####
-# à minimiseer sur la maturité (ici 1 an ?) et sur les ratings
-# un seul appel à cette fonction necessaire pour le calibrage 
-# à calibrer K fois les paramètres k mu et pi0
-# et 1 fois le paramètre sigma (car la volatilité est supposé constante)
-Ecart_JLT <- function(param){
-  e <- 0
-  for (t in 0:TT){ for (i in 1:6){
-    spread <- spread_i_fct(1000, t, TT, param_test, M, D, i,LGD)
-    e <- e + (spread-SpreadMarket[i])^2
-  }}
-  return(e)
-} ## parametre sur le rating
-
-TT <- 1
-LB <- matrix(c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), nrow = 8)
-UB <- matrix(c(2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2), nrow = 8)
-param_tt <- hjkb(param_test,Ecart_JLT,lower=LB,upper=UB)$par
-# encore à améliorer
-
-
-
-
-
-
-
-
-
-
-
-
-##### Calibrage des paramètres ####
+# Calibrage des paramètres
 hist(pit_fct(1000, 3, param_test), freq=FALSE, main='Histogramme des simulations (param random)', xlab='Simulation des lambda t = 3')
 
 Ecart_JLT <- function(param){
@@ -225,12 +219,8 @@ Ecart_JLT <- function(param){
   }
   return(e)
 }
-
 test <- c(1,2,3,4)
 print(test[1:3])
-
-
-
 
 k_test2 <- param_test[,1]
 mu_test2 <- param_test[,2]
