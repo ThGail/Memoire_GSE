b54f7afb636f9db500a025743cb39e46f93e1a5d
Mengru CHEN <99873364+MengruCHEN@users.noreply.github.com>
2022-04-25 23:48:41 +0200

    (reste HW)

--- TOUS LES MODELES.R
+++ TOUS LES MODELES.R
@@ -10,6 +10,7 @@ sheetEIOPA <- read_excel("RFR_for_publication_RFR_curves.xlsx", sheet = 3)
 
 Maturite <- as.numeric(sheetDGlo$`Courbe des taux ZC`[7:156])
 TauxZC <- as.numeric(sheetDGlo$...2[7:156])
+PrixZC <- exp(-Maturite*TauxZC)
 TauxIflZC <- as.numeric(sheetDGlo$...5[7:106])
 PrixCaps <- as.numeric(sheetCali$...5[3:22])
 SpreadMarket  <- as.numeric(sheetDGlo$...15[3:9])
@@ -130,7 +131,7 @@ plotTZC <- TZC_Vas_FF_sim.t(N, tt, 10, paramVas)
 matplot(tt,t(plotTZC[1:10,]),type="l",
         main="Scénarios de taux zéro-coupon de maturité 10 ans",
         xlab="Temps",ylab="TZC")
-plot(colMeans(plotTZC),type="l",lwd=2,col="red",
+plot(tt,colMeans(plotTZC),type="l",lwd=2,col="red",
      main="Moyenne de taux zéro-coupon de maturité 10 ans",
      xlab="Temps",ylab="TZC")
 
@@ -139,7 +140,7 @@ plotPZC <- PZC_Vas_FF_sim.t(N, tt, 10, paramVas)
 matplot(tt,t(plotPZC[1:10,]),type="l",
         main="Scénarios du prix zéro-coupon de maturité 10 ans",
         xlab="Temps",ylab="PZC")
-plot(colMeans(plotPZC),type="l",lwd=2,col="red",
+plot(tt,colMeans(plotPZC),type="l",lwd=2,col="red",
      main="Moyenne du prix zéro-coupon de maturité 10 ans",
      xlab="Temps",ylab="TZC")
 
@@ -363,42 +364,42 @@ param_init = c(0.2,0.2,0.2,0.2)
 LB = c(0,0,0,0)
 UB = c(10,10,10,10)
 
-#AAA : 0.00781250 0.21624298 0.05812742 0.06718750
+#AAA : 1.599722e-01 1.288132e-01 2.030100e-01 2.126438e-13
 spread_mkt <- SpreadMarket[1]
 #paramCIR$AAA <- hjkb(param_init,ecart_CIR,lower=LB,upper=UB)$par
 paramCIR$AAA <- nlminb(start = param_init,ecart_CIR,lower=LB,upper=UB)$par
 #paramCIR$AAA <- optim(par=param_init,fn=ecart_CIR,lower=LB,upper=UB,method="L-BFGS-B")$par
 paramCIR$AAA[3] <- min(paramCIR$AAA[3],sqrt(2*paramCIR$AAA[1]*paramCIR$AAA[2]))
 
-#AA : 0.125000 0.137500 0.185405 0.075000
+#AA : 0.1793781 0.1526860 0.2028968 0.0000000
 spread_mkt <- SpreadMarket[2]
 #paramCIR$AA <- hjkb(param_init,ecart_CIR,lower=LB,upper=UB)$par
 paramCIR$AA <- nlminb(start = param_init,ecart_CIR,lower=LB,upper=UB)$par
 #paramCIR$AA <- optim(par=param_init,fn=ecart_CIR,lower=LB,upper=UB,method="L-BFGS-B")$par
 paramCIR$AA[3] <- min(paramCIR$AA[3],sqrt(2*paramCIR$AA[1]*paramCIR$AA[2]))
 
-#A : 0.0312500 1.2156250 0.2756385 0.0781250
+#A : 0.1960534 0.1731844 0.2027408 0.0000000
 spread_mkt <- SpreadMarket[3]
 #paramCIR$A <- hjkb(param_init,ecart_CIR,lower=LB,upper=UB)$par
 paramCIR$A <- nlminb(start = param_init,ecart_CIR,lower=LB,upper=UB)$par
 #paramCIR$A <- optim(par=param_init,fn=ecart_CIR,lower=LB,upper=UB,method="L-BFGS-B")$par
 paramCIR$A[3] <- min(paramCIR$A[3],sqrt(2*paramCIR$A[1]*paramCIR$A[2]))
 
-#BBB : 0.00390625 0.16676254 0.03609477 0.19609375
+#BBB : 0.19780523 0.17654727 0.20257493 0.01274617
 spread_mkt <- SpreadMarket[4]
 #paramCIR$BBB <- hjkb(param_init,ecart_CIR,lower=LB,upper=UB)$par
 paramCIR$BBB <- nlminb(start = param_init,ecart_CIR,lower=LB,upper=UB)$par
 #paramCIR$BBB <- optim(par=param_init,fn=ecart_CIR,lower=LB,upper=UB,method="L-BFGS-B")$par
 paramCIR$BBB[3] <- min(paramCIR$BBB[3],sqrt(2*paramCIR$BBB[1]*paramCIR$BBB[2]))
 
-#BB : 1.2000000 0.2000000 0.6928203 0.2000000
+#BB : 0.19887557 0.18437281 0.20176218 0.04921644
 spread_mkt <- SpreadMarket[5]
 #paramCIR$BB <- hjkb(param_init,ecart_CIR,lower=LB,upper=UB)$par
 paramCIR$BB <- nlminb(start = param_init,ecart_CIR,lower=LB,upper=UB)$par
 #paramCIR$BB <- optim(par=param_init,fn=ecart_CIR,lower=LB,upper=UB,method="L-BFGS-B")$par
 paramCIR$BB[3] <- min(paramCIR$BB[3],sqrt(2*paramCIR$BB[1]*paramCIR$BB[2]))
 
-#B : 1.6687500 0.2000000 0.8170067 0.2000000
+#B : 0.19890474 0.18459973 0.20173836 0.05140548
 spread_mkt <- SpreadMarket[6]
 #paramCIR$B <- hjkb(param_init,ecart_CIR,lower=LB,upper=UB)$par
 paramCIR$B <- nlminb(start = param_init,ecart_CIR,lower=LB,upper=UB)$par
